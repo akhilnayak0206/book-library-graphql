70f532bbbaa8b3284c16d6d6c2ddc711f4b37174
Akhil D Nayak <37106902+akhilnayak0206@users.noreply.github.com>
2020-04-09 15:02:11 +0530

    setup server

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const graphqlHTTP = require('express-graphql');
+
+const connectDB = require('./config/db');
+
+// const path = require('path');
+
+const app = express();
+
+//Connect Database
+connectDB();
+
+//Init Middleware
+app.use(express.json({ extended: false }));
+
+//Define Routes
+// app.use('/hello', require('./routes/api/hello')); //example
+app.use('/graphql', graphqlHTTP({}));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
