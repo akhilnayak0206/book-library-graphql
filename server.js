fe96ebc4ddf3c57ebbb1e711a85f3b9242e1e9d1
Akhil D Nayak <37106902+akhilnayak0206@users.noreply.github.com>
2020-04-21 19:20:25 +0530

    pre deployment

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const cors = require('cors');
-// const path = require('path');
+const path = require('path');
 
 const connectDB = require('./config/db');
 const schema = require('./schema/schema');
@@ -31,6 +31,17 @@ app.use(
     }),
   })
 );
+
+// Serve static assets i.e. React(Front End) build folder in production
+if (process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static('frontend/build'));
+
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, './frontend/build/index.html'));
+    // changed the above line from path.res(__dirname, 'frontend', 'build', 'index.html')); to allow reloading of the site & load through URL if react-router-dom is added for routing.
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
